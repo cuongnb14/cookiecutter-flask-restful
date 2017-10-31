d8717296bd4886f51b87f7680602079dffd4c741
Cuong Nguyen <cuongnb14@gmail.com>
2017-10-31 21:42:22 +0700

    fix offset and limit for panigator

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/base/paginator.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/base/paginator.py
@@ -10,4 +10,4 @@ class Paginator:
         self.offset = offset if offset else 0
         self.limit = limit if limit else 10
         self.total = sqlalchemy_query.count()
-        self.result = sqlalchemy_query.limit(limit).offset(offset)
+        self.result = sqlalchemy_query.limit(self.limit).offset(self.offset)

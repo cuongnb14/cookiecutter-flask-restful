a4c56c1bf31ece99f90b324dc6fc59710184fee5
Cuong Nguyen <cuongnb14@gmail.com>
2018-02-21 11:29:51 +0700

    update config to more secure

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/configs/config.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/configs/config.py
@@ -3,7 +3,13 @@ from datetime import timedelta
 
 env = os.getenv
 
-SECRET_KEY = "ji8u14b8-j8i9-11e8-bb72-f48e38861a42"
+# SECURITY CONFIGURATION
+# ----------------------------------------------------------
+
+# The secret key is needed to keep the client-side sessions secure. Raises exception if FLASK_SECRET_KEY not configured.
+SECRET_KEY = env('FLASK_SECRET_KEY')
+
+DEBUG = False
 
 # FLASK RESTful CONFIG
 # ----------------------------------------------------------
@@ -28,14 +34,13 @@ CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', 'redis://localhost:6378')
 # Flask-JWT-Extended CONFIG
 # ----------------------------------------------------------
 # TODO: Change it
-JWT_SECRET_KEY = "10f414b8-1093-11e8-bb72-f48e38861a42"
+JWT_SECRET_KEY = env('JWT_SECRET_KEY')
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
 
 # LOGGING CONFIG
 # ----------------------------------------------------------
 LOGGER_NAME = '{{cookiecutter.project_slug}}'
-SLACK_API_KEY = env('SLACK_API_KEY', "xoxp-251214099845-251214099845-251214099845-251214099845251214099845")
-DEBUG = False
+SLACK_API_KEY = env('SLACK_API_KEY')
 LOG_FILE = "logs/api.ERROR.log"
 LOGGING = {
     'version': 1,
